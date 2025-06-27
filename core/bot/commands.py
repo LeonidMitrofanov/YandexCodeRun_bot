30f8c39e482b6671b54d4c7c77bb8524b19a2d2d
Leonid_MacBook <alforfon@gmail.com>
2025-06-27 22:48:34 +0300

    feat: lang_distr

--- core/bot/commands.py
+++ core/bot/commands.py
@@ -1,5 +1,7 @@
 from aiogram import Router, types
 from aiogram.filters import Command
+from io import BytesIO
+from matplotlib import pyplot as plt
 from core.parser import CodeRunRatingScraper
 from core.parser.exceptions import *
 from core.analytics import StatsCalculator, PlotBuilder
@@ -23,16 +25,16 @@ async def cmd_help(message: types.Message):
         reply_markup=help_keyboard
     )
 
-
 @router.message(Command("update"))
 async def cmd_update(message: types.Message):
     if scraper._is_updating:
         await message.answer("🔄 Парсинг уже в процессе, пожалуйста подождите...")
         return
     try:
-        await message.answer("⏳ Начинаем парсинг данных...")
+        progress_msg = await message.answer("⏳ Парсим данные...")
         await scraper.update()
         await message.answer(f"✅ Данные обновлены ({scraper.last_update})")
+        await progress_msg.delete()
     except DataCollectionError as e:
         await message.answer(f"❌ Ошибка при обновлении данных: {str(e)}")
     except Exception as e:
@@ -40,7 +42,45 @@ async def cmd_update(message: types.Message):
 
 @router.message(Command("lang_distr"))
 async def cmd_lang_distr(message: types.Message):
-    await message.answer(f"Распределение участников по языкам: ")
+    try:
+        df = scraper.get_data()
+        if df.empty:
+            await message.answer("Нет данных для построения графиков\n"
+                                 "Выполните /update")
+            return
+
+        progress_msg = await message.answer("⏳ Строим графики...")
+        fig_bar = PlotBuilder.plot_users_by_language_bar(df)
+        fig_pie = PlotBuilder.plot_users_by_language_pie(df)
+
+        def fig_to_bytes(fig: plt.Figure) -> bytes:
+            buf = BytesIO()
+            fig.savefig(buf, format='png', dpi=100, bbox_inches='tight')
+            buf.seek(0)
+            plt.close(fig)
+            return buf.getvalue()
+
+        bar_bytes = fig_to_bytes(fig_bar)
+        pie_bytes = fig_to_bytes(fig_pie)        
+        bar_photo = types.BufferedInputFile(bar_bytes, filename="lang_bar.png")
+        pie_photo = types.BufferedInputFile(pie_bytes, filename="lang_pie.png")
+
+        await message.answer_photo(
+            photo=bar_photo,
+            caption="📊 Распределение участников по языкам (столбчатая диаграмма)"
+        )        
+        await message.answer_photo(
+            photo=pie_photo,
+            caption="🍰 Распределение участников по языкам (круговая диаграмма)"
+        )
+
+        await progress_msg.delete()
+
+    except ValueError as e:
+        await message.answer(f"❌ Ошибка: {str(e)}")
+    except Exception as e:
+        await message.answer(f"⚠️ Произошла непредвиденная ошибка: {str(e)}")
+
 
 def register_commands(dp):
     dp.include_router(router)
