2086b7e8799396d5c9ab712bf4706ce7e905c89e
Leonid_MacBook <alforfon@gmail.com>
2025-06-27 20:57:00 +0300

    feat: data update from bot

--- core/bot/bot.py
+++ core/bot/bot.py
@@ -1,5 +1,4 @@
 from aiogram import Bot, Dispatcher
-from aiogram.enums import ParseMode
 from .config import BotConfig
 
 bot = Bot(
--- core/bot/keyboards.py
+++ core/bot/keyboards.py
@@ -4,7 +4,7 @@ def get_help_keyboard():
     builder = ReplyKeyboardBuilder()
     builder.button(text="/help")
     builder.button(text="/start")
-    builder.button(text="/top")
+    builder.button(text="/lang_distr")
     builder.button(text="/recent")
     builder.adjust(2, 2)  # 2 кнопки в первом ряду, 2 во втором
     return builder.as_markup(resize_keyboard=True)
