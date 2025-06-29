faf6a71192f7cd55331ede09f0cad41fd5c41dd2
Leonid_MacBook <alforfon@bk.ru>
2025-06-29 19:17:34 +0300

    fix: generel standings

--- core/analytics/stats_calculator.py
+++ core/analytics/stats_calculator.py
@@ -1,8 +1,26 @@
 import pandas as pd
+from typing import Dict
 from .config import StatConfig
 
 class StatsCalculator:
     @staticmethod
-    def group_by_user(df: pd.DataFrame) -> pd.DataFrame:
+    def _get_language_columns(prefix: str) -> Dict[str, str]:
+        """Возвращает словарь с колонками для конкретного префикса"""
+        return {
+            f'{prefix}{lang}': 'first'
+            for lang in StatConfig.LANGUAGES
+        }
+
+    @classmethod
+    def _build_agg_config(cls) -> Dict[str, str]:
+        """Генерирует конфигурацию для агрегации данных"""
+        config = {}
+        config.update(cls._get_language_columns('Место_'))
+        config.update(cls._get_language_columns('Баллы_'))
+        config.update(StatConfig.COMMON_COLUMNS)
+        return config
+
+    @classmethod
+    def group_by_user(cls, df: pd.DataFrame) -> pd.DataFrame:
         """Группирует DataFrame по участникам с заданной агрегацией"""
-        return df.groupby('Участник').agg(StatConfig.build_agg_config()).reset_index()
+        return df.groupby('Участник').agg(cls._build_agg_config()).reset_index()
